7152258f40c1795cc614abab95e9cc76fcdb207f
Alex Furdui <furdui.alexandru@isa.utm.md>
2022-02-02 03:54:46 +0200

    initializedd react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Register from "./register";
+// import Reset from "./reset";
+// import Test from './test'
+function App() {
+  return (
+    <div className="app">
+      hello
+    </div>
+  );
+}
+export default App;
